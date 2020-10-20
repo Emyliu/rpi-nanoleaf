09d13104db6ab4f9c1762d446653b54779b38a0c
Edmond Liu <32317817+Emyliu@users.noreply.github.com>
2020-10-20 16:02:57 -0400

    Basic color loop

--- connection.py
+++ connection.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import requests
+
+# Base address for my Nanoleaf
+base_address = "http://192.168.68.129:16021/api/v1/hQfmNSrfeYyNn8i0JoD91qRvpX5DJ3hW/state"
+# API key
+
+while(True):
+    color = input("Enter a number from 1 to 256: ")
+    r = requests.put(base_address, json={ "hue": { "value": int(color) }})
+    print(int(color))
+    print(r.content)
+
