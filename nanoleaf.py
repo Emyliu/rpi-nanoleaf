0f317e484f3fa7c64fb7829f86693dc795882d9f
Edmond Liu <32317817+Emyliu@users.noreply.github.com>
2020-10-20 22:24:07 -0400

    split address of nanoleaf api

--- nanoleaf.py
+++ nanoleaf.py
@@ -1,17 +1,17 @@
 import requests
 
 # Base address for my Nanoleaf
-base_address = "http://192.168.68.129:16021/api/v1/hQfmNSrfeYyNn8i0JoD91qRvpX5DJ3hW/state"
+base_address = "http://192.168.68.129:16021/api/v1/"
 # API key
+api_key = "hQfmNSrfeYyNn8i0JoD91qRvpX5DJ3hW"
 
 def change_color(color):
     print(color)
-    r = requests.put(base_address, json={ "hue": { "value": int(color) }, "sat": { "value": 100 }})
+    r = requests.put(base_address + api_key + "/state", json={ "hue": { "value": int(color) }, "sat": { "value": 100 }})
     return r.content
 
-
 def off():
-    r = requests.put(base_address, json={ "on": { "value": False }})
+    requests.put(base_address + api_key + "/state", json={ "on": { "value": False }})
 
 def white():
-    r = requests.put(base_address + '/sat', json={ "sat": { "value": 0 }})
+    requests.put(base_address + api_key + "/state" + '/sat', json={ "sat": { "value": 0 }})
